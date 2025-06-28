20aba9c82e42e04375adcae04fe340421ff8435a
zhaoyi-1995 <492613075@qq.com>
2025-06-28 18:19:36 +0800

    去除部分预渲染

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    ppr: "incremental"
-  }
 }
 
 export default nextConfig
